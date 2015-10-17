07c7af4c17f8ce33ee20ec4aab7c04e982fd40ef
Philippe Rheaume <trucphil85@hotmail.com>
2015-10-17 17:20:51 -0400

    Algo temporaire
    Algo temporaire dans Feistel et RC4 le temps que je comprenne

--- src/Feistel.java
+++ src/Feistel.java
@@ -7,3 +7,47 @@ public class Feistel {
 	}
 	
 }
+
+//http://www.commentcamarche.net/forum/affich-25066859-cryptography-algorithme-de-feistel-en-java-c
+// Algo temporaire en C trouvé sur le net
+
+char f(char caractere){	//f est une bijection aléatoire ou pseudo-aléatoire 
+                                        // (définition de bijection sur wikipédia =P )
+	return (char) ( 7*((int) caractere ) + 3 ) % 256 
+            //cette formule est vraiment une bijection pourrie pour faire un cryptage  
+            // correct mais tu peux la changer
+}
+
+void simpletour(char* iarray, char* oarray, int size){
+	int cpt = 0;
+	while(cpt<size){
+		oarray[2*cpt] = iarray[2*cpt+1];
+		oarray[2*cpt+1] = iarray[2*cpt] ^ f(iarray[2*cpt+1]);	
+                    //le symbole ^ est le symbole de l'opération logique XOR en c
+		cpt++;
+	}
+}		//la chaine de caractère finale se situe dans la chaine oarray
+
+void multitour(char* iarray, char* oarray, int size, int nbtours){
+	char* temp;
+	while(nbtours > 0){
+		simpletour(iarray, oarray, size);
+		temp = iarray;
+		iarray = oarray;
+		oarray = temp;
+		nbtours--;
+	}	
+}		//la chaine de caractère final se situe dans la chaine iarray
+
+
+//Variables a déclarer dans la fonction main()
+
+int size;		//moitié de la taille de la chaine de caractère d'entrée
+char iarray[2n];	//chaine de caractère d'entrée
+
+char oarray[2n];	//chaine de caractère de résultat
+
+//Exemple d'utilisation (DES) :
+
+multitour(iarray, oarray, size, 16);
+//La chaine iarray contient maintenant une chaine cryptée
--- src/RC4.java
+++ src/RC4.java
@@ -1,9 +1,52 @@
 package TP1Securite;
 
+//http://stackoverflow.com/questions/12289717/rc4-encryption-java
+// Algo temporaire trouvé sur le net et un peu modifié
+
 public class RC4 {
 
-	public RC4(String filecontent) {
-		
-	}
-	
+//	public RC4(String filecontent) {
+
+    private byte[] S = new byte[256];
+    private byte[] T = new byte[256];
+    private int keylen;
+
+    public RC4(byte[] key) {
+        keylen = key.length;
+        for (int i = 0; i < 256; i++) {
+            S[i] = (byte) i;
+            T[i] = key[i % keylen];
+        }
+        
+        int j = 0;
+        byte tmp;
+            
+        for (int i = 0; i < 256; i++) {
+            j = (j + S[i] + T[i]) & 0xFF;
+            tmp = S[j];
+            S[j] = S[i];
+            S[i] = tmp;
+        }
+    }
+
+    public byte[] encrypt(byte[] plaintext) {
+        byte[] ciphertext = new byte[plaintext.length];
+        int i = 0, j = 0, k, t;
+        byte tmp;
+        for (int counter = 0; counter < plaintext.length; counter++) {
+            i = (i + 1) & 0xFF;
+            j = (j + S[i]) & 0xFF;
+            tmp = S[j];
+            S[j] = S[i];
+            S[i] = tmp;
+            t = (S[i] + S[j]) & 0xFF;
+            k = S[t];
+            ciphertext[counter] = (byte) (plaintext[counter] ^ k);
+        }
+        return ciphertext;
+    }
+
+    public byte[] decrypt(byte[] ciphertext) {
+        return encrypt(ciphertext);
+    }
 }
