a657f786ddb6cfd147482b6df660cd4499a53a9b
Philippe Rheaume <trucphil85@hotmail.com>
2015-10-18 14:44:24 -0400

    Algo plus avancé

--- src/RC4.java
+++ src/RC4.java
@@ -1,26 +1,25 @@
 package TP1Securite;
 
 //http://stackoverflow.com/questions/12289717/rc4-encryption-java
-// Algo temporaire trouvé sur le net et un peu modifié
 
 public class RC4 {
 
-//	public RC4(String filecontent) {
-
-    private byte[] S = new byte[256];
-    private byte[] T = new byte[256];
+    private String[] S = new String[256];
+    private String[] T = new String[256];
     private int keylen;
+    private String[] key;
 
-    public RC4(byte[] key) {
+    public RC4() {
+
         keylen = key.length;
         for (int i = 0; i < 256; i++) {
-            S[i] = (byte) i;
+            S[i] = (String) i;
             T[i] = key[i % keylen];
         }
-        
+
         int j = 0;
-        byte tmp;
-            
+        String tmp;
+        
         for (int i = 0; i < 256; i++) {
             j = (j + S[i] + T[i]) & 0xFF;
             tmp = S[j];
@@ -29,10 +28,13 @@ public class RC4 {
         }
     }
 
-    public byte[] encrypt(byte[] plaintext) {
-        byte[] ciphertext = new byte[plaintext.length];
+    public String encrypt(String message) {
+
+    	String plaintext = bitsManager.stringToBits(message); 
+        
+        String ciphertext = new String[plaintext.length];
         int i = 0, j = 0, k, t;
-        byte tmp;
+        String tmp;
         for (int counter = 0; counter < plaintext.length; counter++) {
             i = (i + 1) & 0xFF;
             j = (j + S[i]) & 0xFF;
@@ -41,12 +43,12 @@ public class RC4 {
             S[i] = tmp;
             t = (S[i] + S[j]) & 0xFF;
             k = S[t];
-            ciphertext[counter] = (byte) (plaintext[counter] ^ k);
+            ciphertext[counter] = (String) (plaintext[counter] ^ k);
         }
         return ciphertext;
     }
 
-    public byte[] decrypt(byte[] ciphertext) {
+    public String decrypt(String ciphertext) {
         return encrypt(ciphertext);
     }
 }
