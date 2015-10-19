56ed6c4286a5a3073673cade4704a2776933b6ac
gbastien1 <gabrielle.bastien1@gmail.com>
2015-10-19 15:25:08 -0400

    RC4 marche!!!! (Y) :D

--- src/RC4.java
+++ src/RC4.java
@@ -1,24 +1,24 @@
 package TP1Securite;
 
+
 //Found at http://stackoverflow.com/questions/12289717/rc4-encryption-java
 
 public class RC4 {
 
-    private byte[] S = new byte[256];
-    private byte[] T = new byte[256];
+    private int[] S = new int[256];
+    private int[] T = new int[256];
     private int keylen;
-    private byte[] key;
+    private int[] _key;
 
-    public RC4() {
-
+    public RC4(int[] key) {
         keylen = key.length;
         for (int i = 0; i < 256; i++) {
-            S[i] = (byte) i;
+            S[i] = (int) i;
             T[i] = key[i % keylen];
         }
 
         int j = 0;
-        byte tmp;
+        int tmp;
         
         for (int i = 0; i < 256; i++) {
             j = (j + S[i] + T[i]) & 0xFF;
@@ -33,18 +33,20 @@ public class RC4 {
         
         byte[] ciphertext = new byte[plaintext.length];
         int i = 0, j = 0, k, t;
-        byte tmp;
+        int tmp;
         for (int counter = 0; counter < plaintext.length; counter++) {
             i = (i + 1) & 0xFF;
             j = (j + S[i]) & 0xFF;
+            //swap s[i] & s[j]
             tmp = S[j];
             S[j] = S[i];
             S[i] = tmp;
+            //
             t = (S[i] + S[j]) & 0xFF;
             k = S[t];
             ciphertext[counter] = (byte) (plaintext[counter] ^ k);
         }
-        return ciphertext.toString();
+        return new String(ciphertext);
     }
 
     public String decrypt(String ciphertext) {
